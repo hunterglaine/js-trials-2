da2bedee2e9dfd0f79fb7ea802144bff0a55852b
Hunter Laine <hunterglaine@gmail.com>
2021-01-28 17:01:53 -0800

    Create calculateTotal function

--- functions.js
+++ functions.js
@@ -17,4 +17,24 @@ const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
 
 // 3. calculateTotal
 
-// Define your function here
+// Return the total price of an item, figuring in state taxes and fees.
+
+const calculateTotal = (basePrice, state, tax=0.05) => {
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+    if (state === 'CA'){
+        fee = 0.03 * subtotal;
+    }
+    else if (state === 'PA'){
+        fee = 2;
+    }
+    else if (state === 'MA'){
+        if (basePrice <= 100){
+            fee = 1;
+        }
+        else{
+            fee = 3;
+        }
+    }
+    return subtotal + fee
+}

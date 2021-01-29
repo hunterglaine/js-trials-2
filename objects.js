4c5bd289edb5480e680232fc309ab55d575cbc94
Hunter Laine <hunterglaine@gmail.com>
2021-01-28 17:32:41 -0800

    Create countWords function

--- objects.js
+++ objects.js
@@ -3,7 +3,19 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  // Return a plain object of each word and the no. of times they appear.
+
+  const wordCounts = {}
+
+  for (const word of phrase.split(' ')) {
+    if (wordCounts[word]) {
+      wordCounts[word] += 1;
+    }
+    else {
+      wordCounts[word] = 1;
+    }
+  }
+  return wordCounts;
 }
 
 
